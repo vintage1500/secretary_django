1a6bf82391f5fce1edd58b3b0a92a5a9315c7b15
raul <myemail@mail.ru>
2025-04-13 23:54:23 +0300

    try continue

--- secretary_faculty/faculty_bot/admin.py
+++ secretary_faculty/faculty_bot/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'us_group', 'administrator')
+    search_fields = ('last_name', 'first_name', 'us_group', 'chat_id')
+    list_filter = ('administrator', 'us_group')
+
+@admin.register(QuestionCategory)
+class QuestionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(QuestionSubcategory)
+class QuestionSubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
+
+@admin.register(StaticQuestion)
+class StaticQuestionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'answer')
+    list_filter = ('category',)
+
+@admin.register(DynamicQuestion)
+class DynamicQuestionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'answer')
+    search_fields = ('description',)
+    list_filter = ('category', 'answer')
+    raw_id_fields = ('user',)
--- secretary_faculty/faculty_bot/models.py
+++ secretary_faculty/faculty_bot/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from .managers.obj_manager import UserManager, StaticQuestionManager, DynamicQuestionManager, QuestionCategoryManager, QuestionSubcategoryManager
 
-# Create your models here.
+class QuestionCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    objects = QuestionCategoryManager()
+    
+    def __str__(self):
+        return self.name
+
+class QuestionSubcategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(
+        QuestionCategory, 
+        on_delete=models.CASCADE,
+        related_name='subcategories'
+    )
+    
+    objects = QuestionSubcategoryManager()
+    
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+
+class User(models.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100)
+    us_group = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    administrator = models.BooleanField(default=False)
+    chat_id = models.BigIntegerField(unique=True)
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} ({self.us_group})"
+
+class StaticQuestion(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(
+        QuestionCategory,
+        on_delete=models.CASCADE,
+        related_name='static_questions'
+    )
+    answer = models.TextField()
+    
+    objects = StaticQuestionManager()
+    
+    def __str__(self):
+        return self.name
+
+class DynamicQuestion(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='dynamic_questions'
+    )
+    description = models.TextField()
+    category = models.ForeignKey(
+        QuestionCategory,
+        on_delete=models.CASCADE,
+        related_name='dynamic_questions'
+    )
+    answer = models.BooleanField(default=False)
+    
+    objects = DynamicQuestionManager()
+    
+    def __str__(self):
+        return f"Вопрос от {self.user} ({self.category.name})"
